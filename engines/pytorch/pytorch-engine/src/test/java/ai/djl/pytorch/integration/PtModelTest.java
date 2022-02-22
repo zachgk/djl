0a1cc269d793c9f609a73e303e7ad3c2f0cf9640
Zach Kimberg <kimbergz@amazon.com>
2022-02-22 09:51:22 -0800

    [WIP] Debugging newPredictor
    Change-Id: Ic22a6a89ede42f8e96951f88414bc7784c4a8d10

--- engines/pytorch/pytorch-engine/src/test/java/ai/djl/pytorch/integration/PtModelTest.java
+++ engines/pytorch/pytorch-engine/src/test/java/ai/djl/pytorch/integration/PtModelTest.java
@@ -13,21 +13,34 @@
 package ai.djl.pytorch.integration;
 
 import ai.djl.Device;
+import ai.djl.MalformedModelException;
 import ai.djl.Model;
 import ai.djl.ModelException;
 import ai.djl.inference.Predictor;
+import ai.djl.modality.Classifications;
+import ai.djl.modality.Input;
+import ai.djl.modality.Output;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.ImageFactory;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDList;
+import ai.djl.ndarray.NDManager;
+import ai.djl.ndarray.types.DataType;
 import ai.djl.ndarray.types.Shape;
 import ai.djl.pytorch.engine.PtModel;
 import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ModelNotFoundException;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.training.util.ProgressBar;
 import ai.djl.translate.NoopTranslator;
 import ai.djl.translate.TranslateException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -54,5 +67,33 @@ public class PtModelTest {
                 }
             }
         }
+    }
+
+    @Test
+    public void testGpuPredictor()
+        throws ModelNotFoundException, MalformedModelException, IOException, TranslateException {
+        Criteria<Input, Output> criteria =
+            Criteria.builder()
+                .setTypes(Input.class, Output.class)
+                .optModelUrls("djl://ai.djl.pytorch/resnet/0.0.1/traced_resnet18")
+                .optProgress(new ProgressBar())
+                .optDevice(Device.cpu())
+                .build();
+        try(ZooModel<Input, Output> model = criteria.loadModel()) {
+            try(Predictor<Input, Output> predictor = model.newPredictor(Device.cpu())) {
+                try( NDManager man = model.getNDManager().newSubManager(Device.cpu())) {
+                    // Path path = Paths.get("/Users/kimbergz/Downloads/zeros_1_3_224_224.ndlist");
+                    // NDArray array = man.decode(new FileInputStream(path.toFile()));
+                    NDArray array = man.ones(new Shape(3, 224, 224), DataType.UINT8);
+                    Image img = ImageFactory.getInstance().fromNDArray(array);
+                    Path imgPath = Paths.get("/tmp/testFile");
+                    img.save(new FileOutputStream(imgPath.toFile()), "png");
+                    Input input = new Input();
+                    input.add(Files.readAllBytes(imgPath));
+                    predictor.predict(input);
+                }
+            }
+        }
+
     }
 }
